57e9e7f46e80646761e795453cea5b06bb547445
Valeria <vsantanamegia.cf@iesesteveterradas.cat>
2024-12-16 17:54:10 +0100

    Eliminación de carpeta java-R

---
+++ Deures/Deures00/src/main/java/com/exercicis/Exercici0000.java
@@ -0,0 +1,15 @@
+package com.exercicis;
+
+import java.util.Scanner;
+
+public class Exercici0000 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        /*
+            TODO: Resol aquí l'exercici
+         */
+
+        scanner.close();
+    }
+}
